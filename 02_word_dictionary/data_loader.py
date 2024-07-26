bc29ff8bfd5b00a0d8e17197b69d4d5f3da27db8
shinnew99 <yoojinshin9918@gmail.com>
2024-07-26 19:14:45 +0900

    update Node1 and Node2 data_download.py

--- 02_word_dictionary/data_loader.py
+++ 02_word_dictionary/data_loader.py
@@ -1,19 +1,35 @@
 import os
 
-class DataLoader:
-    def __init__(self, path):
-        self.path = path
-        self.raw_data = self._load_data()
+# class DataLoader:
+#     def __init__(self, path):
+#         self.path = path
+#         self.raw_data = self._load_data()
 
-    def _load_data(self):
-        with open(self.path, "r") as f:
+#     def _load_data(self):
+#         with open(self.path, "r") as f:
+#             raw = f.read().splitlines()
+#         return raw
+
+#     def get_data(self):
+#         return self.raw_data
+
+#     def print_examples(self, num_examples=5, step=20):
+#         print("Example:")
+#         for sen in self.raw_data[0:num_examples * step:step]:
+#             print(">>", sen)
+
+class DataLoader:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def get_data(self):
+        with open(self.file_path, 'r', encoding='utf-8') as f:
             raw = f.read().splitlines()
         return raw
 
-    def get_data(self):
-        return self.raw_data
-
-    def print_examples(self, num_examples=5, step=20):
+    def print_examples(self, num_examples=5):
+        raw_data = self.get_data()
+        print("Data Size:", len(raw_data))
         print("Example:")
-        for sen in self.raw_data[0:num_examples * step:step]:
+        for sen in raw_data[:num_examples]:
             print(">>", sen)
