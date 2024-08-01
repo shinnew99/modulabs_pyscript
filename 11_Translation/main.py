cd21676f8e0eae08aad38f89fec82ddc25f6af68
shinnew99 <yoojinshin9918@gmail.com>
2024-08-01 16:22:44 +0900

    update the whole changes in 11_Translation

--- 11_Translation/main.py
+++ 11_Translation/main.py
@@ -1,69 +1,260 @@
 import tensorflow as tf
 import numpy as np
 import os
+import re
+
 from sklearn.model_selection import train_test_split
-from preprocessing.text_preprocessing import TextPreprocessing
-from models.encoder import Encoder
-from models.decoder import Decoder
-from models.seq2seq import Seq2SeqModel
-from utils import tokenize, loss_function
-from tqdm import tqdm
+
+import matplotlib.ticker as ticker
+import matplotlib.pyplot as plt
+
+import time
+import io
+
+print(tf.__version__)
+
+path_to_zip = tf.keras.utils.get_file(
+    'spa-eng.zip',
+    origin='http://storage.googleapis.com/download.tensorflow.org/data/spa-eng.zip',
+    extract=True)
+
+path_to_file = os.path.dirname(path_to_zip) + "/spa-eng/spa.txt"
+
+# Specify the encoding explicitly
+with open(path_to_file, "r", encoding="utf-8") as f:
+    raw = f.read().splitlines()
+
+print("Data Size:", len(raw))
+print("Example:")
+
+for sen in raw[0:100][::20]: 
+    print(">>", sen)
+
+def preprocess_sentence(sentence, s_token=False, e_token=False):
+    sentence = sentence.lower().strip()
+
+    sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
+    sentence = re.sub(r'[" "]+', " ", sentence)
+    sentence = re.sub(r"[^a-zA-Z?.!,]+", " ", sentence)
+
+    sentence = sentence.strip()
+
+    if s_token:
+        sentence = '<start> ' + sentence
+
+    if e_token:
+        sentence += ' <end>'
+    
+    return sentence
+
+print("슝~")
+
+
+enc_corpus = []
+dec_corpus = []
+
+num_examples = 30000
+
+for pair in raw[:num_examples]:
+    eng, spa = pair.split("\t")
+
+    enc_corpus.append(preprocess_sentence(eng))
+    dec_corpus.append(preprocess_sentence(spa, s_token=True, e_token=True))
+
+print("English:", enc_corpus[100])   # go away !
+print("Spanish:", dec_corpus[100])   # <start> salga de aqu ! <end>
+
+
+def tokenize(corpus):
+    tokenizer = tf.keras.preprocessing.text.Tokenizer(filters='')
+    tokenizer.fit_on_texts(corpus)
+
+    tensor = tokenizer.texts_to_sequences(corpus)
+
+    tensor = tf.keras.preprocessing.sequence.pad_sequences(tensor, padding='post')
+
+    return tensor, tokenizer
+
+# Tokenize and split the data
+enc_tensor, enc_tokenizer = tokenize(enc_corpus)
+dec_tensor, dec_tokenizer = tokenize(dec_corpus)
+
+enc_train, enc_val, dec_train, dec_val = train_test_split(enc_tensor, dec_tensor, test_size=0.2)
+
+class BahdanauAttention(tf.keras.layers.Layer):
+    def __init__(self, units):
+        super(BahdanauAttention, self).__init__()
+        self.w_dec = tf.keras.layers.Dense(units)
+        self.w_enc = tf.keras.layers.Dense(units)
+        self.w_com = tf.keras.layers.Dense(1)
+    
+    def call(self, h_enc, h_dec):
+        # h_enc shape: [batch x length x units]
+        # h_dec shape: [batch x units]
+
+        h_enc = self.w_enc(h_enc)
+        h_dec = tf.expand_dims(h_dec, 1)
+        h_dec = self.w_dec(h_dec)
+
+        score = self.w_com(tf.nn.tanh(h_dec + h_enc))
+        
+        attn = tf.nn.softmax(score, axis=1)
+
+        context_vec = attn * h_enc
+        context_vec = tf.reduce_sum(context_vec, axis=1)
+
+        return context_vec, attn
+
+print("슝~")
+
+class Encoder(tf.keras.Model):
+    def __init__(self, vocab_size, embedding_dim, enc_units):
+        super(Encoder, self).__init__()
+        
+        self.enc_units = enc_units
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self.gru = tf.keras.layers.GRU(enc_units,
+                                       return_sequences=True)
+        
+    def call(self, x):
+        out = self.embedding(x)
+        out = self.gru(out)
+        
+        return out
+
+
+class Decoder(tf.keras.Model):
+    def __init__(self, vocab_size, embedding_dim, dec_units):
+        super(Decoder, self).__init__()
+        self.dec_units = dec_units
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self.gru = tf.keras.layers.GRU(dec_units,
+                                       return_sequences=True,
+                                       return_state=True)
+        self.fc = tf.keras.layers.Dense(vocab_size)
+
+        self.attention = BahdanauAttention(self.dec_units)
+
+    def call(self, x, h_dec, enc_out):
+        context_vec, attn = self.attention(enc_out, h_dec)
+
+        out = self.embedding(x)
+        out = tf.concat([tf.expand_dims(context_vec, 1), out], axis=-1)
+        
+        out, h_dec = self.gru(out)
+        out = tf.reshape(out, (-1, out.shape[2]))
+        out = self.fc(out)
+
+        return out, h_dec, attn
+
+optimizer = tf.keras.optimizers.Adam()
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+    from_logits=True, reduction='none')
+
+def loss_function(real, pred):
+    mask = tf.math.logical_not(tf.math.equal(real, 0))
+    loss = loss_object(real, pred)
+    
+    mask = tf.cast(mask, dtype=loss.dtype)
+    loss *= mask
+    
+    return tf.reduce_mean(loss)
+
+print("슝~")
+
+
+@tf.function
+def train_step(src, tgt, encoder, decoder, optimizer, dec_tok):
+    bsz = src.shape[0]
+    loss = 0
+
+    with tf.GradientTape() as tape:
+        enc_out = encoder(src)
+        h_dec = enc_out[:, -1]
+        
+        dec_src = tf.expand_dims([dec_tok.word_index['<start>']] * bsz, 1)
+
+        for t in range(1, tgt.shape[1]):
+            pred, h_dec, _ = decoder(dec_src, h_dec, enc_out)
+
+            loss += loss_function(tgt[:, t], pred)
+            dec_src = tf.expand_dims(tgt[:, t], 1)
+        
+    batch_loss = (loss / int(tgt.shape[1]))
+
+    variables = encoder.trainable_variables + decoder.trainable_variables
+    gradients = tape.gradient(loss, variables)
+    optimizer.apply_gradients(zip(gradients, variables))
+    
+    return batch_loss
+
+print("슝~")
+
+from tqdm import tqdm    # tqdm
 import random
 
-def main():
-    print(tf.__version__)
+EPOCHS = 10
+BATCH_SIZE = 64  # Define a batch size
 
-    path_to_zip = tf.keras.utils.get_file(
-        'spa-eng.zip',
-        origin='http://storage.googleapis.com/download.tensorflow.org/data/spa-eng.zip',
-        extract=True)
+encoder = Encoder(vocab_size=len(enc_tokenizer.word_index)+1,
+                  embedding_dim=256,
+                  enc_units=512)
+
+decoder = Decoder(vocab_size=len(dec_tokenizer.word_index)+1,
+                  embedding_dim=256,
+                  dec_units=512)
+
+for epoch in range(EPOCHS):
+    total_loss = 0
     
-    path_to_file = os.path.dirname(path_to_zip) + "/spa-eng/spa.txt"
+    idx_list = list(range(0, enc_train.shape[0], BATCH_SIZE))
+    random.shuffle(idx_list)
+    t = tqdm(idx_list)    # tqdm
 
-    with open(path_to_file, "r") as f:
-        raw = f.read().splitlines()
-
-    enc_corpus = []
-    dec_corpus = []
-
-    num_examples = 30000
-    for pair in raw[:num_examples]:
-        eng, spa = pair.split("\t")
-        enc_corpus.append(TextPreprocessing.preprocess_sentence(eng))
-        dec_corpus.append(TextPreprocessing.preprocess_sentence(spa, s_token=True, e_token=True))
-
-    enc_tensor, enc_tokenizer = tokenize(enc_corpus)
-    dec_tensor, dec_tokenizer = tokenize(dec_corpus)
-
-    enc_train, enc_val, dec_train, dec_val = train_test_split(enc_tensor, dec_tensor, test_size=0.2)
-
-    vocab_size = len(enc_tokenizer.word_index) + 1
-    emb_dim = 256
-    enc_units = 512
-    dec_units = 512
-    BATCH_SIZE = 64
-    EPOCHS = 10
-
-    encoder = Encoder(vocab_size, emb_dim, enc_units)
-    decoder = Decoder(vocab_size, emb_dim, dec_units)
-    optimizer = tf.keras.optimizers.Adam()
-    seq2seq = Seq2SeqModel(encoder, decoder, optimizer, loss_function)
-
-    for epoch in range(EPOCHS):
-        total_loss = 0
-        idx_list = list(range(0, enc_train.shape[0], BATCH_SIZE))
-        random.shuffle(idx_list)
-        t = tqdm(idx_list)
-
-        for (batch, idx) in enumerate(t):
-            batch_loss = seq2seq.train_step(
-                enc_train[idx:idx+BATCH_SIZE],
-                dec_train[idx:idx+BATCH_SIZE],
-                dec_tokenizer)
+    for (batch, idx) in enumerate(t):
+        batch_loss = train_step(enc_train[idx:idx+BATCH_SIZE],
+                                dec_train[idx:idx+BATCH_SIZE],
+                                encoder,
+                                decoder,
+                                optimizer,
+                                dec_tokenizer)
     
-            total_loss += batch_loss
-            t.set_description_str('Epoch %2d' % (epoch + 1))
-            t.set_postfix_str('Loss %.4f' % (total_loss.numpy() / (batch + 1)))
+        total_loss += batch_loss
+        
+        t.set_description_str('Epoch %2d' % (epoch + 1))    # tqdm
+        t.set_postfix_str('Loss %.4f' % (total_loss.numpy() / (batch + 1)))    # tqdm
 
-if __name__ == "__main__":
-    main()
+# Function to translate a sentence
+def translate_sentence(sentence, encoder, decoder, enc_tokenizer, dec_tokenizer):
+    sentence = preprocess_sentence(sentence)
+
+    inputs = [enc_tokenizer.word_index[i] for i in sentence.split(' ')]
+    inputs = tf.keras.preprocessing.sequence.pad_sequences([inputs], maxlen=enc_train.shape[1], padding='post')
+    inputs = tf.convert_to_tensor(inputs)
+
+    enc_out = encoder(inputs)
+    h_dec = enc_out[:, -1]
+
+    dec_input = tf.expand_dims([dec_tokenizer.word_index['<start>']], 0)
+    result = ''
+
+    for t in range(dec_train.shape[1]):
+        predictions, h_dec, attention_weights = decoder(dec_input, h_dec, enc_out)
+
+        predicted_id = tf.argmax(predictions[0]).numpy()
+
+        result += dec_tokenizer.index_word[predicted_id] + ' '
+
+        if dec_tokenizer.index_word[predicted_id] == '<end>':
+            return result, sentence
+
+        dec_input = tf.expand_dims([predicted_id], 0)
+
+    return result, sentence
+
+# Translate and print the results for a few sentences
+sentences = ["Go away!", "Are you coming?", "I'm hungry."]
+for sentence in sentences:
+    result, sentence = translate_sentence(sentence, encoder, decoder, enc_tokenizer, dec_tokenizer)
+    print(f'Input: {sentence}')
+    print(f'Translated: {result}')
